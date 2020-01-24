80655d7b7c33064c032343bc29f568e680568dfa
Andreas Askjem <andaskjem@gmail.com>
2020-01-24 15:34:03 +0100

    Initial commit

---
+++ Person.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace Modul3Oblig1
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int BirthYear { get; set; }
+        public int DeathYear { get; set; }
+
+        public List<Person> Children = new List<Person>();
+
+        private Person _father;
+        public Person Father {
+            get => _father;
+            set
+            {
+                _father = value;
+                value.Children.Add(this);
+            }
+        }
+
+        private Person _mother;
+        public Person Mother
+        {
+            get => _mother;
+            set
+            {
+                _mother = value;
+                value.Children.Add(this);
+            }
+        }
+
+        internal void SkrivInfoListe()
+        {
+            var output = $"ID: {Id}, Fornavn: {FirstName}";
+
+            //if (LastName != null) { output += $", Etternvan: {LastName}"; }
+
+            output += $", Fødselsår: {BirthYear}";
+            if (DeathYear != null) { output += $", Dødsår: {DeathYear}"; }
+
+            if (_father != null) { output += $", Far ID: {_father.Id}"; }
+            if (_mother != null) { output += $", Mor ID: {_mother.Id}"; }
+
+            Console.WriteLine("  " + output);
+        }
+
+        public void SkrivInfoEnkeltperson()
+        {
+            var output = $"  ID: {Id}, Fornavn: {FirstName}";
+
+            if (LastName != null) { output += $", Etternvan: {LastName}"; }
+
+            output += $", Fødselsår: {BirthYear}\n";
+
+            if(_father != null || _mother != null) {
+                output += "  Foreldre:\n";
+                if (_father != null) { output += $"    Far: {_father.FirstName}, ID: {_father.Id}\n"; }
+                if (_mother != null) { output += $"    Mor: {_mother.FirstName}, ID: {_mother.Id}\n"; }
+            }
+
+            if (Children.Count > 0)
+            {
+                output += "  Barn:\n";
+                foreach (var child in Children)
+                {
+                    output += $"    {child.FirstName}, (ID: {child.Id})\n";
+                }
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Modul3Oblig1
+{
+    class Program
+    {
+        static void Main()
+        {
+            var liste = SetUpPersonObjects();
+
+            while (true)
+            {
+                Console.Write("Input: ");
+                var input = Console.ReadLine();
+
+                if (input.Length == 0)
+                {
+                    continue;
+                }
+
+                if (input == "hjelp")
+                {
+                    ShowHelp();
+                }
+                else if (input == "liste")
+                {
+                    foreach (var person in liste)
+                    {
+                        person.SkrivInfoListe();
+                    }
+                }
+                else if (input.Substring(0, 4) == "vis ")
+                {
+                    var indexStr = input.Substring(4);
+
+                    if (!indexStr.All(char.IsDigit))
+                    {
+                        Console.WriteLine($"  ID må være et tall mellom 1 og {liste.Count}.");
+                        continue;
+                    }
+                    var index = int.Parse(indexStr) - 1;
+                    if (index >= liste.Count)
+                    {
+                        Console.WriteLine($"  ID må være et tall mellom 1 og {liste.Count}.");
+                        continue;
+                    }
+
+                    liste[index].SkrivInfoEnkeltperson();
+                }
+                else
+                {
+                    Console.WriteLine("  Ikke godkjent input. Skriv \"hjelp\" for å se alternativer.");
+                }
+            }
+        }
+
+        private static List<Person> SetUpPersonObjects()
+        {
+            var sverreMagnus = new Person { Id = 1, FirstName = "Sverre Magnus", BirthYear = 2005 };
+            var ingridAlexandra = new Person { Id = 2, FirstName = "Ingrid Alexandra", BirthYear = 2004 };
+            var haakon = new Person { Id = 3, FirstName = "Haakon Magnus", BirthYear = 1973 };
+            var metteMarit = new Person { Id = 4, FirstName = "Mette-Marit", BirthYear = 1973 };
+            var marius = new Person { Id = 5, FirstName = "Marius", LastName = "Borg Høiby", BirthYear = 1997 };
+            var harald = new Person { Id = 6, FirstName = "Harald", BirthYear = 1937 };
+            var sonja = new Person { Id = 7, FirstName = "Sonja", BirthYear = 1937 };
+            var olav = new Person { Id = 8, FirstName = "Olav", BirthYear = 1903 };
+
+            sverreMagnus.Father = haakon;
+            sverreMagnus.Mother = metteMarit;
+            ingridAlexandra.Father = haakon;
+            ingridAlexandra.Mother = metteMarit;
+            marius.Mother = metteMarit;
+            haakon.Father = harald;
+            haakon.Mother = sonja;
+            harald.Father = olav;
+
+            var liste = new List<Person>
+            {
+                sverreMagnus,
+                ingridAlexandra,
+                haakon,
+                metteMarit,
+                marius,
+                harald,
+                sonja,
+                olav
+            };
+
+            return liste;
+        }
+
+        private static void ShowHelp()
+        {
+            const string text = "  hjelp => Vis tilgjengelige kommandoer og hva de gjør.\n" +
+                                "  liste => Lister alle personer med id, fornavn, fødselsår, dødsår og navn og id på foreldre som er registrert.\n" +
+                                "  vis < id > => Viser en bestemt person med navn og ID for foreldre og barn";
+            Console.WriteLine(text);
+        }
+    }
+}
